6324c7af3a6263859f12b51e9c2b83ac855fe743
Rafael Castellanos-Welsh <rafaelcastellanos-welsh@Rafaels-MacBook-Pro.local>
2021-12-27 21:23:22 -0800

    moved types to folder

---
+++ types/CoffeeShopsType.ts
@@ -0,0 +1,7 @@
+export type CoffeeShopsType = {
+  id: string,
+  name: string,
+  roaster: boolean,
+  city: string,
+  state: string,
+}
---
+++ types/index.ts
@@ -0,0 +1,1 @@
+export type {CoffeeShopsType} from './CoffeeShopsType'
