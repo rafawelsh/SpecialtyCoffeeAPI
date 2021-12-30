dbf7a3e4256f59d0b49e6e1e0642492784f7ed03
Rafael Castellanos-Welsh <rafaelcastellanos-welsh@Rafaels-MacBook-Pro.local>
2021-12-29 21:11:46 -0800

    applied some styles to likesCounter

--- components/LikesCounter/LikesCounter.tsx
+++ components/LikesCounter/LikesCounter.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./likesCounter.module.css";
 import { CounterType } from "../../types/CoffeeShopsType";
 
+import { FaArrowUp, FaArrowDown } from "react-icons/fa";
+
 export default function LikesCounter({
 	likes,
 	dislikes,
@@ -9,12 +11,22 @@ export default function LikesCounter({
 }: CounterType) {
 	return (
 		<div className={styles.container}>
-			<button id='likes' onClick={handleClick}>
-				Click: {likes}
+			<button
+				id='likes'
+				className={`${styles.arrowButton} ${styles.buttonStyles}`}
+				onClick={handleClick}
+			>
+				Like
 			</button>
-			<div>0</div>
-			<button id='dislikes' onClick={handleClick}>
-				Click: {dislikes}
+			<div className={`${styles.likesIndicator} ${styles.buttonStyle}`}>
+				{likes} / {dislikes}
+			</div>
+			<button
+				id='dislikes'
+				className={`${styles.arrowButton} ${styles.buttonStyles}`}
+				onClick={handleClick}
+			>
+				Dislikes
 			</button>
 		</div>
 	);
